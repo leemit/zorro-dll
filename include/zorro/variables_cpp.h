45b8ac428946088f1daf9ddbc4f3fc7e0cb8562f
xezon <pascalkross@gmail.com>
2017-10-19 20:42:19 +0200

    Move variable builder macros into var.h

--- include/zorro/variables_cpp.h
+++ include/zorro/variables_cpp.h
@@ -3,33 +3,6 @@
 
 #include "zorro/var.h"
 
-#define ZORRO_BUILD_VARIABLE_TYPE(type, name, link) \
-struct S##name##Variable : SVariableBaseDef<type> { \
-	inline type& get() const { return (link); } \
-	inline void set(const type& value) { (link) = value; } \
-};
-
-#define ZORRO_BUILD_EXPRESSION_TYPE(type, name, link) \
-struct S##name##Expression : SVariableBaseDef<type> { \
-	inline TType get() const { return (link); } \
-};
-
-#ifdef ZORRO_IMPL
-#define ZORRO_BUILD_VARIABLE(type, name, link) \
-	ZORRO_BUILD_VARIABLE_TYPE(type, name, link) \
-	CVariable<S##name##Variable>& name = CVariable<S##name##Variable>::getInstance();
-#define ZORRO_BUILD_EXPRESSION(type, name, link) \
-	ZORRO_BUILD_EXPRESSION_TYPE(type, name, link) \
-	CExpression<S##name##Expression>& name = CExpression<S##name##Expression>::getInstance();
-#else
-#define ZORRO_BUILD_VARIABLE(type, name, link) \
-	ZORRO_BUILD_VARIABLE_TYPE(type, name, link) \
-	extern CVariable<S##name##Variable>& name;
-#define ZORRO_BUILD_EXPRESSION(type, name, link) \
-	ZORRO_BUILD_EXPRESSION_TYPE(type, name, link) \
-	extern CExpression<S##name##Expression>& name;
-#endif
-
 ZORRO_NAMESPACE_OPEN
 
 ZORRO_BUILD_VARIABLE(var, Slider1, g->vSlider[1])
