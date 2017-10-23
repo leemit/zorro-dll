feccf93edf5a2e822342e3f138a50fe806a4c3fa
xezon <pascalkross@gmail.com>
2017-10-23 20:37:52 +0200

    Move windows headers into zorro.h

--- include/zorro/zorro_common.h
+++ include/zorro/zorro_common.h
@@ -1,18 +1,6 @@
 
 #pragma once
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
-#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-
-#include <windows.h>
-#include <wtypes.h>
-#include <stdio.h>
-#include <math.h>
-
 #define ZORRO_CALL __cdecl
 #define ZORRO_EXPORT extern "C" __declspec(dllexport)
 
